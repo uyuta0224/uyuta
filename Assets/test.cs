74014bed0e1b96df63eb00ddee9a1981fc42f48c
Uyuta0224 <yuta.feb.0224@me.com>
2014-05-09 16:38:32 +0900

    変更２
    変更しました

---
+++ Assets/test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	static float ie=-0.01f;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0,1,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ie=ie*(-1);
+		for (int i = 0;i<10; i++){
+			transform.position = new Vector3(0,i*ie,0);
+		}
+	}
+}
